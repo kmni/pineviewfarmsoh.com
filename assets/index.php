9b019e6b3cd515e4b9f9da0901aec03d62f6ecd6
Curtis LaMalfa <curtis@kmni.com>
2013-07-03 12:17:56 -0700

    Initial Commit

---
+++ assets/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Pineview Farms</title>
+        <meta charset="utf-8" />
+        <link rel="Shortcut Icon" type="image/ico" href="assets/images/favicon.png" />
+        <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+        <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+        <script type="text/javascript" src="assets/js/theme.js"></script>
+        <script type="text/javascript" src="assets/js/rounded.js"></script>
+        <!--[if IE 7]><link rel="stylesheet" href="assets/css/ie/ie7.css" type="text/css" media="screen"><![endif]-->
+        <!--[if lt IE 7]><link rel="stylesheet" href="assets/css/ie/ie6.css" type="text/css" media="screen"><![endif]-->
+        <!--[if lt IE 6]><script type="text/javascript">DD_roundies.addRule('.pngfix');</script><![endif]-->
+    </head>
+    <body>
+
+        <div id="outer-wrapper">
+            
+            <div id="inner-wrapper">
+                <div id="left-content" class="floatleft">
+                    <div id="logo">
+                        <a href="#" title=""><img src="assets/images/logo.png" alt="" /></a>
+                    </div>
+                    
+                    <div id="nav">
+                        <ul>
+                            <li><a href="" title="">Our Mares</a></li>
+                            <li><a href="" title="">Foal Showcase</a></li>
+                            <li><a href="" title="">Sale Horses</a></li>
+                            <li><a href="" title="">Our Farm</a></li>
+                            <li><a href="" title="">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    
+                    <div id="link-blocks">
+                        <p>Legendary Stallions</p>
+                        <div id="image">
+                            <a href="#"><img src="assets/images/horse-1.png" alt="" /></a>
+                        </div>
+                        <a href="#"><span class="title">Invitation Only</span></a>
+                    </div>
+                    <div id="link-blocks-two">
+                        <div id="image">
+                            <a href="#"><img src="assets/images/horse-2.png" alt="" /></a>
+                        </div>
+                        <a href="#"><span class="title">Lazy Loper</span></a>
+                    </div>
+                    <div id="link-blocks-three">
+                        <div id="image">
+                            <a href="#"><img src="assets/images/horse-3.png" alt="" /></a>
+                        </div>
+                        <a href="#"><span class="title">Winnie's Willy</span></a>
+                    </div>
+                </div><!--/left-content  -->
+                
+                <div id="right-content" class="floatleft">
+                    <div id="welcome">
+                        <img src="assets/images/welcome-text.jpg" alt="" />
+                    </div>
+                    <div id="content-image">
+                        <img src="assets/images/content-image.jpg" alt="" />
+                    </div>
+                    <div id="content">
+                        <p>
+                            <span class="uppercase">L</span>orem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.<br/><br/>
+
+Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.<br/><br/>
+
+Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.<br/><br/>
+
+Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.<br/><br/>
+
+Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.<br/><br/>
+
+Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.<br/><br/>
+
+Sed adipiscing ornare risus. Morbi est est, blandit sit amet, sagittis vel, euismod vel, velit. Pellentesque egestas sem. Suspendisse commodo ullamcorper magna.
+
+                        </p>
+                    </div>
+                </div><!--/right-content  -->
+            </div>
+            
+        </div><!-- /outer-wrapper -->
+
+
+
+    </body>
+</html>
